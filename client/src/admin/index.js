2ed520b46cb3326de71166e72fea541d643ad030
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-15 16:02:30 +0000

    populating divs instead of ul:

--- client/src/admin/index.js
+++ client/src/admin/index.js
@@ -3,13 +3,25 @@ var quizServer = require('../models/quizServer');
 window.onload = function() {
   var allQuizzes;
 
-  quizServer.getAllQuizzes( function( data ) {
-    allQuizzes = data;
-    var allQuizzesList = document.getElementById( 'all-quizzes-list' );
-    allQuizzes.forEach( function( quiz ) {
-      var li = document.createElement( 'li' );
-      li.innerText = quiz.title;
-      allQuizzesList.appendChild( li );
-    });
+  quizServer.getAllQuizzes( function( allQuizzes ) {
+    var numberPerRow = 3;
+    var quizzesContainer = document.getElementById( 'quizzes-container' );
+    var i = 0;
+    var rowCount = 0;
+    while( i < allQuizzes.length ) {
+      var rowDiv = document.createElement( 'div' );
+      rowDiv.classList.add( 'quiz-row' );
+      while( rowCount < numberPerRow && i < allQuizzes.length ) {
+        var quiz = allQuizzes[i];
+        var div = document.createElement( 'div' );
+        div.innerText = quiz.title;
+        div.classList.add( 'quiz-item' );
+        rowDiv.appendChild( div );
+        rowCount++;
+        i++;
+      }
+      quizzesContainer.appendChild( rowDiv );
+      rowCount = 0;
+    }
   });
 }
