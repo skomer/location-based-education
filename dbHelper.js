96480a63b5f188633c5d6f1f3f629934827f8671
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-15 12:16:15 +0000

    refactored dbhelper

--- dbHelper.js
+++ dbHelper.js
@@ -2,12 +2,19 @@ var MongoClient = require('mongodb').MongoClient;
 
 var dbHelper = {
 
-  createQuiz: function(title, callback){
+  getDatabase: function(callback){
     var url = 'mongodb://localhost:27017/location_based_education';
     MongoClient.connect(url, function(err, db){
-      if(err){
-        console.error(err);
-      }else{
+      callback( err, db );
+    });
+  },
+
+  createQuiz: function(title, callback){
+    this.getDatabase(function( err, db ){
+      if ( err ) {
+        console.error( err );
+      }
+      else {
         var quizzesCollection = db.collection('quizzes');
         quizzesCollection.insert({
           title: title
@@ -16,12 +23,14 @@ var dbHelper = {
       callback( err );
     });
   },
+
   getAllQuizzes: function(callback){
-    var url = 'mongodb://localhost:27017/location_based_education';
-    MongoClient.connect(url, function(err, db){
-      if(err){
-        console.error(err);
-      }else{
+    this.getDatabase(function( err, db ){
+      if ( err ) {
+        console.error( err );
+        callback( err, null );
+      }
+      else {
         var quizzesCollection = db.collection('quizzes');
         quizzesCollection.find({}).toArray(function(err, allQuizzesArray){
           console.error(err);
