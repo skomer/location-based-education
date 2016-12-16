1bb08ab68b88ae807bd647dd6f153b51bbc0d712
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-16 16:27:56 +0000

    user quiz document can see all quiz titles

--- client/src/user/index.js
+++ client/src/user/index.js
@@ -1,3 +1,18 @@
-window.onload = function(){
-  console.log("user index.js loaded");
+var quizServer = require('../models/quizServer');
+var QuizListView = require('../views/quizListView');
+
+window.onload = function() {
+
+  var allQuizzes;
+
+  quizServer.getAllQuizzes( function( allQuizzes ) {
+    var listItems = allQuizzes.map(function(quiz){
+      return {
+        title: quiz.title
+      };
+    });
+    var quizzesContainer = document.getElementById('quizzes-container');
+    var quizListView = new QuizListView(quizzesContainer);
+    quizListView.populate(listItems);
+  });
 };
