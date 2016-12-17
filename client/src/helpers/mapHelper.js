7ee8d6982e8984ecb51a9fce5abb8299e1bd2233
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-17 12:23:28 +0000

    adding basic map

--- client/src/helpers/mapHelper.js
+++ client/src/helpers/mapHelper.js
@@ -1,16 +1,28 @@
-var MapHelper = function(container, lat, lng, defaultZoom){
-  var center = {
-    lat: lat,
-    lng: lng
-  };
+var values = require( '../values.js' );
 
-  this.map = new google.maps.Map(
-    container,
-    {
-      center: center,
-      zoom: defaultZoom
-    }
-  );
+var MapHelper = function(container, lat, lng, defaultZoom){
+
+  // load gogle maps api
+  var script = document.createElement( 'script' );
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?' +
+      'key=' + values.googleMapsApiKey + '&callback=createMap';
+  document.body.appendChild( script );
+
+  // global function workaround
+  createMap = function() {
+    var defaultCenter = {
+      lat: lat,
+      lng: lng
+    };
+
+    this.map = new google.maps.Map(
+      container,
+      {
+        center: defaultCenter, zoom: defaultZoom
+      }
+    );
+  }
 };
 
 module.exports = MapHelper;
