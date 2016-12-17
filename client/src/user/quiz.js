7d8a6aca7013317cfb6198056e9a54fb15bc0912
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-17 10:54:20 +0000

    fetching quiz object working

--- client/src/user/quiz.js
+++ client/src/user/quiz.js
@@ -4,4 +4,8 @@ window.onload = function() {
   var lastSlashIndex = window.location.href.lastIndexOf( '/' );
   var quizId = window.location.href.substr( lastSlashIndex + 1 );
   console.log("quiz id:", quizId);
+
+  quizServer.getQuizById( quizId, function(quiz){
+    console.log("Quiz:", quiz);
+  });
 };
