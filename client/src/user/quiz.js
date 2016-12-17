178694bdf4958ed39affa66f279de01ae26d373f
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-17 11:21:32 +0000

    grabbing dom elements and setting quiz title

--- client/src/user/quiz.js
+++ client/src/user/quiz.js
@@ -1,11 +1,21 @@
 var quizServer = require('../models/quizServer');
 
 window.onload = function() {
+  var quizTitleP = document.getElementById('quiz-title');
+  var mapContainer = document.getElementById('map-container');
+  var questionTextP = document.getElementById('question-text');
+  var answerTextP = document.getElementById('answer-text');
+  var progressTextP = document.getElementById('progress-text');
+  var nextResultsButton = document.getElementById('next-results-button');
+
   var lastSlashIndex = window.location.href.lastIndexOf( '/' );
   var quizId = window.location.href.substr( lastSlashIndex + 1 );
   console.log("quiz id:", quizId);
 
   quizServer.getQuizById( quizId, function(quiz){
-    console.log("Quiz:", quiz);
+    console.log("fetched quiz:", quiz);
+    globalQuiz = quiz;
+    quizTitleP.innerText = quiz.title;
+
   });
 };
