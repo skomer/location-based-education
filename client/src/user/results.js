52c8eb23a74f8f88eb08fea1b2a19c5f9d289c6b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-17 16:41:19 +0000

    adding quiz title

--- client/src/user/results.js
+++ client/src/user/results.js
@@ -1,9 +1,13 @@
 var quizServer = require( '../models/quizServer' );
 
 window.onload = function() {
+  var quizTitleH2 = document.getElementById( 'quiz-title' );
+
   var answers = [ 'AF', 'BB', 'AF', 'FR' ];
   quizServer.getQuizById( '-KZCcaO1hToqpMhfHnls', function( quiz ) {
     console.log( "quiz questions:", quiz.questions );
     console.log( "answers:", answers );
+
+    quizTitleH2.innerText = quiz.title;
   });
 };
