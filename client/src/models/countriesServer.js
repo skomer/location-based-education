24b6518c224c7aa9814041d442f87295da4964b1
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-17 17:30:24 +0000

    adding nameForCode()

--- client/src/models/countriesServer.js
+++ client/src/models/countriesServer.js
@@ -9,6 +9,12 @@ var CountriesServer = function( callback ) {
 };
 
 CountriesServer.prototype = {
+  nameForCode: function( countryCode ) {
+    var foundCountry = this.countries.find( function( country ) {
+      return country.code === countryCode;
+    });
+    return foundCountry.name;
+  }
 };
 
 module.exports = CountriesServer;
