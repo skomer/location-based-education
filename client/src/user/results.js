a697c6b32ab6b6862e4f51bad89700f82f108504
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-17 17:30:47 +0000

    adding basic display of results

--- client/src/user/results.js
+++ client/src/user/results.js
@@ -1,14 +1,32 @@
-var quizServer = require( '../models/quizServer' );
+var CountriesServer = require('../models/countriesServer');
+var quizServer = require('../models/quizServer');
 
 window.onload = function() {
   var quizTitleH2 = document.getElementById( 'quiz-title' );
-  var resultsTableBody = document.getElementById( 'results-table' );
+  var resultsTableBody = document.getElementById( 'results-table-body' );
 
-  var answers = [ 'AF', 'BB', 'AF', 'FR' ];
-  quizServer.getQuizById( '-KZCcaO1hToqpMhfHnls', function( quiz ) {
-    console.log( "quiz questions:", quiz.questions );
-    console.log( "answers:", answers );
+  var userAnswers = [ 'AF', 'BB', 'AF', 'FR' ];
 
-    quizTitleH2.innerText = quiz.title;
+  var countriesServer = new CountriesServer( function() {
+    quizServer.getQuizById( '-KZCcaO1hToqpMhfHnls', function( quiz ) {
+      console.log( "quiz questions:", quiz.questions );
+      console.log( "user answers:", userAnswers );
+
+      quizTitleH2.innerText = quiz.title;
+      quiz.questions.forEach( function( question, index ) {
+        var questionText = question.text;
+        var userAnswer = countriesServer.nameForCode( userAnswers[index] );
+        var correctAnswer = countriesServer.nameForCode( question.answer );
+
+        var resultRow = document.createElement( 'tr' );
+        var questionTextTd = document.createElement( 'td' );
+        questionTextTd.innerText = questionText;
+        var resultTd = document.createElement( 'td' );
+        resultTd.innerText = "user answer: " + userAnswer + ", correct answer: " + correctAnswer;
+        resultRow.appendChild( questionTextTd );
+        resultRow.appendChild( resultTd );
+        resultsTableBody.appendChild( resultRow );
+      });
+    });
   });
 };
