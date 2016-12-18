c5fd5c393e0c15f2ee1ff8c03caeaa4f7107aaff
Josephine Elder <parusmontanus@gmail.com>
2016-12-18 12:19:34 +0000

    Added gradient

--- client/build/user/progressBar/progressBar.js
+++ client/build/user/progressBar/progressBar.js
@@ -3,11 +3,11 @@ function progressBar() {
 
   var nextButton = document.getElementById( 'pretend-next-button' );
   
-  var bgWidth = 45;
+  var bgWidth = 50;
   var bgHeight = 500;
 
   nextButton.onclick = function() {
-    draw(10, 4);
+    draw(10, 8);
   };
 
   function draw(segments, position) {
@@ -17,20 +17,18 @@ function progressBar() {
     var pWidth = bgWidth;
     var pHeight = bgHeight / segments;
 
+    var grd = ctx.createLinearGradient(0, pHeight, 0, pHeight * position);
+    grd.addColorStop(0, 'green');
+    grd.addColorStop(1, 'white');
+
     // background rectangle
     ctx.lineWidth = 3;
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, bgWidth, bgHeight);
 
     // progress rectangle
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = grd;
     ctx.fillRect(0, pHeight * (segments - position), pWidth, pHeight * position);
-    console.log(
-      "x: " + 0
-      + "\ny: " + (pHeight * (segments - position))
-      + "\npWidth: " + pWidth
-      + "\npHeight*position: " + (pHeight * position)
-      );
 
   };
 
