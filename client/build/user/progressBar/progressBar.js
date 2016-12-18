270d44ddb151b2189b8e81c5969e3f42a52b4053
Josephine Elder <parusmontanus@gmail.com>
2016-12-18 11:33:47 +0000

    Can load a progress segment from the bottom of the bar

--- client/build/user/progressBar/progressBar.js
+++ client/build/user/progressBar/progressBar.js
@@ -3,8 +3,9 @@ function progressBar() {
 
   var nextButton = document.getElementById( 'pretend-next-button' );
   
-  
-  
+  var bgWidth = 45;
+  var bgHeight = 500;
+
   nextButton.onclick = function() {
     draw(10, 6);
   };
@@ -13,9 +14,21 @@ function progressBar() {
     var canvas = document.getElementById('progress-bar');
     var ctx = canvas.getContext('2d');
 
+    var pWidth = bgWidth;
+    var pHeight = bgHeight / segments;
+
+    // background rectangle
     ctx.lineWidth = 3;
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 45, 500);
+    ctx.fillRect(0, 0, bgWidth, bgHeight);
+
+    // progress rectangle
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0 + bgHeight - pHeight, pWidth, pHeight);
+
+
+
+
   };
 
 
