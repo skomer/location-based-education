9d9e6a88c19ba9a0653082ae42700d379d2f26c0
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-19 16:34:56 +0000

    creating html in javascript

--- client/src/views/infoBoxView.js
+++ client/src/views/infoBoxView.js
@@ -1,11 +1,20 @@
 var InfoBoxView = function() {
-  this.div = document.getElementById( 'infobox-div' );
-  this.textP = document.getElementById( 'infobox-text' );
 
-  this.closeButton = document.getElementById( 'infobox-close-button' );
-  this.closeButton.onclick = function() {
+  this.textP = document.createElement( 'p' );
+  this.textP.id = "infobox-text"
+
+  var closeButton = document.createElement( 'p' );
+  closeButton.id = 'infobox-close-button';
+  closeButton.innerText = "X";
+  closeButton.onclick = function() {
     this.div.style.display = 'none';
   }.bind( this );
+
+  this.div = document.createElement( 'div' );
+  this.div.id = "infobox-div";
+  this.div.appendChild( this.textP );
+  this.div.appendChild( closeButton );
+  document.body.appendChild( this.div );
 };
 
 InfoBoxView.prototype = {
