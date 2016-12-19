b2c08e1f7b3426d22520006943c0a7657f37ec29
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-19 11:34:39 +0000

    added current question text dislplay

--- client/src/user/quiz.js
+++ client/src/user/quiz.js
@@ -2,11 +2,15 @@ var quizServer = require('../models/quizServer');
 var MapHelper = require('../helpers/mapHelper');
 
 var answerTextP;
+var questionTextP;
+var quiz;
+var currentQuestionIndex = 0;
+var userAnswers = [];
 
 window.onload = function() {
   var quizTitleP = document.getElementById('quiz-title');
   var mapContainer = document.getElementById('map-container');
-  var questionTextP = document.getElementById('question-text');
+  questionTextP = document.getElementById('question-text');
   answerTextP = document.getElementById('answer-text');
   var progressTextP = document.getElementById('progress-text');
   var nextResultsButton = document.getElementById('next-results-button');
@@ -15,14 +19,20 @@ window.onload = function() {
   var quizId = window.location.href.substr( lastSlashIndex + 1 );
   console.log("quiz id:", quizId);
 
-  quizServer.getQuizById( quizId, function(quiz){
+  quizServer.getQuizById( quizId, function(fetchedQuiz){
+    quiz = fetchedQuiz;
     console.log("fetched quiz:", quiz);
-    globalQuiz = quiz;
     quizTitleP.innerText = quiz.title;
-    var mapHelper = new MapHelper(mapContainer, 55.9, -3.1, 4, mapClicked);  
+    var mapHelper = new MapHelper(mapContainer, 55.9, -3.1, 4, mapClicked); 
+    updateQuestionDisplay(); 
   });
 };
 
 var mapClicked = function(countryCode, countryName){
   answerTextP.innerText = "Your answer is: " + countryName;
-}
+};
+
+var updateQuestionDisplay = function(){
+  var currentQuestionText = quiz.questions[currentQuestionIndex].text;
+  questionTextP.innerText = currentQuestionText;
+};
