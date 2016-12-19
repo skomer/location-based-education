c52d92fabbcaa2635899b1a2622a58acacbda78b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-19 21:43:44 +0000

    added and passing for title passed in constructor

--- client/src/models/quiz.js
+++ client/src/models/quiz.js
@@ -1,28 +1,8 @@
-var Quiz = function(title, fullResponse){
-  this.title = title;
-  this.fullResponse =[{
-    question: fullResponse.question,
-    answer: fullResponse.answer,
-    countryCode: fullResponse.countryCode,
-    countryName: fullResponse.countryName,
-    archived: fullResponse.true
-  }];
-
-  this.quizQuestions = [];
- // this.published = false;
+var Quiz = function(params){
+  this.title = params.title;
 }
 
 Quiz.prototype = {
-  getSize: function(){
-    this.quizQuestions.length;
-  },
-
-add: function(item){
-  this.quizQuestions.push(item);
-}
 };
 
-
-
-  
 module.exports = Quiz;
