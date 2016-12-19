f227959dce46fd31bf1bfb57e2bd5f814f3bc7cd
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-19 11:44:53 +0000

    diabled next country button until selected

--- client/src/user/quiz.js
+++ client/src/user/quiz.js
@@ -3,6 +3,7 @@ var MapHelper = require('../helpers/mapHelper');
 
 var answerTextP;
 var questionTextP;
+var nextResultsButton;
 var quiz;
 var currentQuestionIndex = 0;
 var userAnswers = [];
@@ -13,7 +14,7 @@ window.onload = function() {
   questionTextP = document.getElementById('question-text');
   answerTextP = document.getElementById('answer-text');
   var progressTextP = document.getElementById('progress-text');
-  var nextResultsButton = document.getElementById('next-results-button');
+  nextResultsButton = document.getElementById('next-results-button');
 
   var lastSlashIndex = window.location.href.lastIndexOf( '/' );
   var quizId = window.location.href.substr( lastSlashIndex + 1 );
@@ -24,15 +25,19 @@ window.onload = function() {
     console.log("fetched quiz:", quiz);
     quizTitleP.innerText = quiz.title;
     var mapHelper = new MapHelper(mapContainer, 55.9, -3.1, 4, mapClicked); 
-    updateQuestionDisplay(); 
+    loadQuestion(currentQuestionIndex); 
   });
 };
 
 var mapClicked = function(countryCode, countryName){
   answerTextP.innerText = "Your answer is: " + countryName;
+  nextResultsButton.disabled = false;
 };
 
-var updateQuestionDisplay = function(){
-  var currentQuestionText = quiz.questions[currentQuestionIndex].text;
+var loadQuestion = function(questionIndex){
+  console.log("loading question:", questionIndex);
+  var currentQuestionText = quiz.questions[questionIndex].text;
   questionTextP.innerText = currentQuestionText;
+  nextResultsButton.disabled = true;
 };
+
