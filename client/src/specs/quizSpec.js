e994ef791399ab0bc028570d18330c011602a565
Cyrus <cdbalsara@icloud.com>
2016-12-19 17:48:37 +0000

    tested that quiz starts empty

--- client/src/specs/quizSpec.js
+++ client/src/specs/quizSpec.js
@@ -29,7 +29,10 @@ beforeEach(function(){
 
   // ******** tests for empty quiz
 
-  
+  it("should have no questions when created with no data", function(){
+    assert.equal(0, quiz1.quizQuestions.length)
+
+  });
 
   it("should have empty string title when created with no data");
 
