388980c353662f1aa658b92e4b694e725fec1611
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-19 12:21:19 +0000

    results page merged into take quiz page

--- client/src/user/quiz.js
+++ client/src/user/quiz.js
@@ -1,14 +1,36 @@
 var quizServer = require('../models/quizServer');
 var MapHelper = require('../helpers/mapHelper');
 
+// Main divs
+var takeQuizDiv;
+var resultsDiv;
+
+// TAKE QUIZ DIV
 var answerTextP;
 var questionTextP;
 var nextResultsButton;
 var quiz;
 var currentQuestionIndex = 0;
 var userAnswers = [];
+var currentAnswer;
+
+// RESULTS DIV
+// functions
+var updateScoreTd;
+var scoreQuestion;
+
+// DOM elements
+var scoreP;
+var scoreOutOf;
+
+// variables
+var score = 0;
 
 window.onload = function() {
+  var takeQuizDiv = document.getElementById( 'take-quiz-container' );
+  var resultsDiv = document.getElementById( 'results-container' );
+  resultsDiv.style.display = 'none';
+
   var quizTitleP = document.getElementById('quiz-title');
   var mapContainer = document.getElementById('map-container');
   questionTextP = document.getElementById('question-text');
@@ -25,12 +47,17 @@ window.onload = function() {
     var numberOfQuestions = quiz.questions.length;
     console.log("fetched quiz:", quiz);
     quizTitleP.innerText = quiz.title;
-    var mapHelper = new MapHelper(mapContainer, 55.9, -3.1, 4, mapClicked); 
-    loadQuestion(currentQuestionIndex); 
+    var mapHelper = new MapHelper(mapContainer, 55.9, -3.1, 4, mapClicked);
+    loadQuestion(currentQuestionIndex);
     nextResultsButton.onclick = function(){
+      userAnswers.push( currentAnswer );
       currentQuestionIndex++;
       if(currentQuestionIndex < numberOfQuestions){
         loadQuestion(currentQuestionIndex);
+      } else {
+        takeQuizDiv.style.display = 'none';
+        resultsDiv.style.display = 'block';
+        showResults();
       }
     }
   });
@@ -39,6 +66,10 @@ window.onload = function() {
 var mapClicked = function(countryCode, countryName){
   answerTextP.innerText = "Your answer is: " + countryName;
   nextResultsButton.disabled = false;
+  currentAnswer = {
+    countryCode: countryCode,
+    countryName: countryName
+  };
 };
 
 var loadQuestion = function(questionIndex){
@@ -48,3 +79,98 @@ var loadQuestion = function(questionIndex){
   nextResultsButton.disabled = true;
 };
 
+var showResults = function() {
+  var resultsTableBody = document.getElementById( 'results-table-body' );
+
+  console.log( "user answers:", userAnswers );
+
+  var marks = [];
+
+  quiz.questions.forEach( function( question, index ) {
+    var questionText = question.text;
+    var userAnswerCode = userAnswers[index].countryCode;
+    var userAnswerName = userAnswers[index].countryName;
+    var correctAnswerCode = question.countryCode;
+    var correctAnswerName = question.countryName;
+
+    var resultRow = document.createElement( 'tr' );
+    var questionNumber = (index + 1).toString();
+    resultRow.id = 'question-' + questionNumber;
+    var questionTextTd = document.createElement( 'td' );
+    questionTextTd.innerText = "Q" + questionNumber + ". " + questionText;
+    var resultTd = document.createElement( 'td' );
+    resultTd.innerText = userAnswerName;
+    resultTd.classList.add( 'answer' );
+
+    resultRow.appendChild( questionTextTd );
+    resultRow.appendChild( resultTd );
+    resultsTableBody.appendChild( resultRow );
+
+    marks[index] = {
+      questionNumber: questionNumber,
+      td: resultTd,
+      correct: userAnswerCode === correctAnswerCode,
+      correctAnswer: correctAnswerName
+    };
+  });
+
+  var totalTextTd = document.createElement( 'td' );
+  totalTextTd.classList.add( 'empty-cell' );
+  totalTextTd.innerText = "";
+  scoreP = document.getElementById( 'score-p' );
+  scoreOutOf = quiz.questions.length.toString();
+  updateScore( 0 );
+
+  var totalScoreTr = document.createElement( 'tr' );
+  totalScoreTr.appendChild( totalTextTd );
+  resultsTableBody.appendChild( totalScoreTr );
+
+  var i = 1;
+  for ( aMark of marks ) {
+    (function( mark ) {
+      scoreQuestion( mark , i * 1000 );
+    })( aMark );
+    i++;
+  }
+
+  setTimeout( function() {
+    window.scrollTo( 0, 0 );
+  }, i * 1000 );
+};
+
+var updateScore = function( scoreIncrease ) {
+  score += scoreIncrease;
+  scoreP.innerText = score.toString() + " / " + scoreOutOf;
+  if ( scoreIncrease > 0 ) {
+    scoreP.classList.remove( 'pulse-green' );
+    setTimeout( function() {
+      scoreP.classList.add( 'pulse-green' );
+    }, 100 );
+  }
+};
+
+var scoreQuestion = function( mark, timeoutLength ) {
+  setTimeout( function () {
+    var td = mark.td;
+    var correct = mark.correct;
+    var correctAnswer = mark.correctAnswer;
+
+    window.location.href = "#";
+    window.location.href = "#question-" + mark.questionNumber;
+
+    if ( correct ) {
+      td.innerText += " ✔";
+      td.classList.add( "correct-answer" );
+      updateScore( 1 );
+    }
+    else {
+      td.innerText += " ✘";
+      td.classList.add( "wrong-answer" );
+      td.classList.add( "tooltip" );
+      var correctAnswerSpan = document.createElement( 'span' );
+      correctAnswerSpan.innerText = correctAnswer;
+      correctAnswerSpan.classList.add( 'tooltip-text' );
+      td.appendChild( correctAnswerSpan );
+    }
+  }, timeoutLength);
+};
