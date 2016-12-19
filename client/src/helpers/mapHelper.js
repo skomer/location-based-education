caacd81ae2bbb62556c3b42d83a2ddf6c3178257
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-19 11:16:32 +0000

    put api key in script tag for simplicity

--- client/src/helpers/mapHelper.js
+++ client/src/helpers/mapHelper.js
@@ -1,43 +1,29 @@
 var values = require( '../values.js' );
 
-var MapHelper = function(container, lat, lng, defaultZoom, helperCallback){
+var MapHelper = function(container, lat, lng, defaultZoom){
+  this.geocoder = new google.maps.Geocoder();
+  var defaultCenter = {
+    lat: lat,
+    lng: lng
+  };
 
-  // load gogle maps api
-  var script = document.createElement( 'script' );
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?' +
-      'key=' + values.googleMapsApiKey + '&callback=createMap';
-  document.body.appendChild( script );
-
-  // global function workaround
-  createMap = function( callback ) {
-    this.geocoder = new google.maps.Geocoder();
-    var defaultCenter = {
-      lat: lat,
-      lng: lng
+  this.map = new google.maps.Map(
+    container,
+    {
+      center: defaultCenter, zoom: defaultZoom
+    }
+  );
+  google.maps.event.addListener(this.map, "click", function(ev){
+    var latLng = {
+      lat: ev.latLng.lat(),
+      lng: ev.latLng.lng()
     };
+    console.log("map clicked at", latLng);
+    console.log(this);
+    this.decodeCountry(latLng, function(){
 
-    this.map = new google.maps.Map(
-      container,
-      {
-        center: defaultCenter, zoom: defaultZoom
-      }
-    );
-    google.maps.event.addListener(this.map, "click", function(ev){
-      var latLng = {
-        lat: ev.latLng.lat(),
-        lng: ev.latLng.lng()
-      };
-      console.log("map clicked at", latLng);
-      console.log(this);
-      this.decodeCountry(latLng, function(){
-
-      });
-    }.bind(this));
-
-    helperCallback();
-
-  }.bind(this);
+    });
+  }.bind(this));
 };
 
 MapHelper.prototype = {
