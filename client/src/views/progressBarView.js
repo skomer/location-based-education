5c4e02ea7a4fed04e755e92664846285865aeeb4
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-20 17:53:30 +0000

    restyling progress bar

--- client/src/views/progressBarView.js
+++ client/src/views/progressBarView.js
@@ -18,26 +18,25 @@ ProgressBarView.prototype = {
     var pWidth = bgWidth;
     var pHeight = bgHeight / this.totalSegments;
 
-    // var grd = this.ctx.createLinearGradient(0, pHeight, 0, pHeight * position);
-    // grd.addColorStop(0, 'rgb(0, 179, 0)');
-    // grd.addColorStop(1, 'rgb(230, 255, 230)');
-
-    // background rectangle
+    // draw backgroud
     this.ctx.fillStyle = 'white';
     this.ctx.fillRect(0, 0, bgWidth, bgHeight);
 
-    // progress rectangle
-    // ctx.lineWidth = 3;
-    // this.ctx.fillStyle = grd;
-    this.ctx.fillStyle = 'limegreen';
-    this.ctx.fillRect(0, pHeight * (this.totalSegments - position), pWidth, pHeight * position);
-
-    // this.ctx.fillStyle = 'white';
-    this.ctx.fillStyle = 'limegreen';
-
+    //draw bars
     for (var i = 0; i < position; i++) {
-      this.ctx.fillRect(0, (i * pHeight) + (pHeight * (this.totalSegments - position)), pWidth, 2);
+      if ( i === position - 1 ) {
+        this.ctx.fillStyle = 'rgb(40, 121, 78)';
+      }
+      else {
+        this.ctx.fillStyle = 'darkseagreen';
+      }
+      this.ctx.fillRect( 0, bgHeight - ( (i + 1) * pHeight), pWidth, pHeight);
+    }
 
+    //draw lines
+    for (var i = 0; i < this.totalSegments; i++ ) {
+      this.ctx.fillStyle = 'skyblue';
+      this.ctx.fillRect( 0, i * pHeight, pWidth, 4 );
     }
   }
 };
